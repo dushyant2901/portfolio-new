95d6f4754eb3ae4e57d7833b84a25aa81498ef8c
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2024-07-27 11:01:27 +0530

    feat: hero section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,18 +24,33 @@ function headerShadow() {
   }
 }
 /* ----- TYPING EFFECT ----- */
-
+var typingEffect = new Typed(".typedText", {
+  strings: ["Designer", "Youtuber", "Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
+});
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
 /* -- HOME -- */
+sr.reveal(".featured-text-card", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
 
 /* -- PROJECT BOX -- */
-
+sr.reveal(".project-box", { interval: 200 });
 /* -- HEADINGS -- */
-
+sr.reveal(".top-header", {});
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-
-/* -- ABOUT INFO & CONTACT INFO -- */
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 
